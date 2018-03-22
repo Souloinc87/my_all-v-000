1e2e003ab16d1ee4264cc194425634d6d16b8cee
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-22 03:58:13 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,6 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
+    if block_return_values.include?(false)
+      false
+    else
+      true
+  end 
 end
-binding.pry
-my_all?([1,2,3]) {|i| i < 2}
+
